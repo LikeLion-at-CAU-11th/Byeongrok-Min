49a9ea3a47d7b0fbe036a110734d492d41d784df
devrokket <rokany@cau.ac.kr>
2023-04-07 17:10:59 +0900

    week5 스탠다드, 챌린지 과제 완료

--- backend-session/config/settings.py
+++ backend-session/config/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -130,11 +130,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False # DB에 반영되기 위해 False로 변경
 
 
 # Static files (CSS, JavaScript, Images)
--- backend-session/posts/models.py
+++ backend-session/posts/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(verbose_name="작성일시", auto_now_add=True)
+    created_at = models.DateTimeField(verbose_name="작성일시", auto_now_add=True) # 챌린지 과제: filter orm을 이용하기. 더미데이터를 만들어서 get으로 불러오는데 시간을 조건으로 해서 불러오기. 
     updated_at = models.DateTimeField(verbose_name="수정일시", auto_now=True)
 
     class Meta:
         abstract = True
 
+# Django 모델에서 필드, 즉 칼럼은 하나의 클래스로 생성된다. 
 class Post(BaseModel):
 
     CHOICES = (
@@ -24,4 +25,4 @@ class Post(BaseModel):
 class Comment(BaseModel):
     writer = models.CharField(verbose_name="작성자", max_length=30)
     content = models.CharField(verbose_name="내용", max_length=200)
-    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, blank=False)
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, blank=False) # 댓글은 일대다 관계이다. comment는 Post를 참조하고 있다. 
