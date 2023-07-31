42598b285b62ac85dd1b58abf312e9d923acabff
devrokket <dev2@Devrokket.local>
2023-08-01 03:17:08 +0900

    chore: 주석 삭제

--- backend-session/posts/urls.py
+++ backend-session/posts/urls.py
@@ -11,38 +11,3 @@ urlpatterns = [
     path('<int:id>/comment/', CommentList.as_view()), # 해당 게시물(post)의 comment 처리. id == post_id
     path('comment/<int:id>', CommentDetail.as_view()) # 해당 comment 처리. id == comment_id
 ]
-
-# mixins view
-# urlpatterns = [
-#     path('', PosListMixins.as_view()),
-#     path('<int:pk>/', PostDetailMixins.as_view()),
-#     path('<int:pk>/comment/', CommentListMixins.as_view()),
-#     path('comment/<int:pk>/', CommentDetailMinxins.as_view())
-# ]
-
-# genericsAPIView
-# <int:id>가 아닌 <int:pk>인 것을 생각.
-# urlpatterns = [
-#     path('', PostListGenericAPIView.as_view()),
-#     path('<int:pk>/', PostDetailGenericAPIView.as_view()),
-#     path('<int:pk>/comment/', CommetListGenericAPIView.as_view()),
-#     path('comment/<int:pk>', CommentDetailGenericAPIView.as_view())
-# ]
-
-# viewSet
-# urlpatterns = [
-#     path('', post_list),
-#     path('<int:pk>/', post_detail_vs),
-#     path('<int:pk>/comment', comment_list),
-#     path('comment/<int:pk>', comment_detail_vs)
-# ]
-
-# router = DefaultRouter()
-# router.register('', PostViewSet)
-# router.register('comment', CommentViewSet, basename='comment router')
-
-# comment_router = DefaultRouter()
-
-# urlpatterns = [
-#     path('', include(router.urls)), 
-# ]
